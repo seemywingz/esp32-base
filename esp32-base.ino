45d11367381cd4f653baa30e719833fe3412aa19
seemywings <kevin.jayne@icloud.com>
2024-04-19 03:33:23 -0400

    wifi working

--- esp32-base.ino
+++ esp32-base.ino
@@ -1,16 +1,51 @@
 
+#include <ESPWiFi.h>
 #include <IOPin.h>
+#include <SPIFFS.h>
 
 IOPin led(8, OUTPUT);
 
+// Web Server
+const String webServerName = "ESP32-C3";
+ESPWiFi wifi(webServerName, "abcd1234");
+
 void setup() {
-  Serial.begin(921600);
+  Serial.begin(115200);
   Serial.println("Hello, ESP32!");
+  initializeWebServer();
 }
 
 void loop() {
+  wifi.handleClient();
+  Serial.println("Blinking LED");
   led.off();
-  delay(100);
+  delay(1000);
   led.on();
-  delay(100);
+  delay(1000);
+}
+
+void initializeWebServer() {
+  // List all files in the LittleFS
+  //   wifi->webServer.on("/files", HTTP_GET, []() {
+  //     File root = SPIFFS.open("/");
+  //     if (!root) {
+  //       wifi->webServer.send(500, "text/html", "Failed to open directory");
+  //       return;
+  //     }
+  //     if (!root.isDirectory()) {
+  //       wifi->webServer.send(500, "text/html", "No directory found");
+  //       return;
+  //     }
+
+  //     String message = "Files on SPIFFS:<br>";
+  //     File file = root.openNextFile();
+  //     while (file) {
+  //       String fileName = file.name();
+  //       message += "<a href=\"" + fileName + "\">" + fileName + "</a><br>";
+  //       file = root.openNextFile();
+  //     }
+  //     wifi->webServer.send(200, "text/html", message);
+  //   });
+  wifi.enableMDNS(webServerName);
+  wifi.start();
 }
