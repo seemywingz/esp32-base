aabb0de2fbccf5a67e423d40b225fa6b3c47d492
seemywings <kevin.jayne@icloud.com>
2024-04-21 13:55:32 -0400

    simple

--- esp32-base.ino
+++ esp32-base.ino
@@ -1,28 +1,13 @@
 
 #include <ESPWiFi.h>
-#include <IOPin.h>
-#include <LittleFS.h>
-
-IOPin led(8, OUTPUT);
 
 // Web Server
 const String webServerName = "ESP32-C3";
 ESPWiFi wifi(webServerName, "abcd1234");
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Hello, ESP32!");
-  initializeWebServer();
-}
-
-void loop() { wifi.handleClient(); }
-
-void initializeWebServer() {
-  wifi.setConnectSubroutine([]() {
-    Serial.println("Connecting to WiFi");
-    Serial.println(wifi.ssid + " " + wifi.password);
-    delay(500);
-  });
   wifi.enableMDNS(webServerName);
   wifi.start();
 }
+
+void loop() { wifi.handleClient(); }
